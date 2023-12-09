f527ad602ce5be304061421d8d25b3244b5a8d32
Serhii Filatov <filser.spirit@gmail.com>
2023-12-09 16:17:14 +0200

    Adds problem solving

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,12 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+const string: KeyValuePair<string, string> = { key: "name", value: "Ihor" };
+const number: KeyValuePair<string, number> = { key: "age", value: 31 };
 
 export {};
